f4be44c02c6bfbc77b39098c6a75f2f202099d1b
Victor Larsen <victorlarsen84@hotmail.com>
2017-10-21 16:05:06 +0200

    Utökade printmeddelandet

--- src/com/victorlarsen/Main.java
+++ src/com/victorlarsen/Main.java
@@ -3,7 +3,7 @@ package com.victorlarsen;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Testar git!");
+        System.out.println("Testar git IGEN!");
 	// write your code here
     }
 }
